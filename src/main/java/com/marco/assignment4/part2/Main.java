7407c1bc2df670d5188b7970c540b5eeab746a8a
Marco <marcoross37@gmail.com>
2019-03-16 23:29:19 +0200

    Comment

--- src/main/java/com/marco/assignment4/part2/Main.java
+++ src/main/java/com/marco/assignment4/part2/Main.java
@@ -1,7 +1,7 @@
 package com.marco.assignment4.part2;
 
 public class Main {
-
+//Part 2 Demonstrates Delegation, Abstraction and Composition
     public static void main(String[] args) {
         Main main = new Main();
         Driver driver = main.newDriver();
